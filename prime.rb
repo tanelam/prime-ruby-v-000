c6a58864a0f81981cbc2e5ca847594d41b5b6273
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-11-06 03:21:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(num)
-  if n <= 1
+  if num <= 1
     false
-  elsif n == 2
+  elsif num == 2
     true
   else
-    (2..n/2).none? { |x| n % x == 0}
-  end 
+    (2..num/2).none? { |x| n % x == 0}
+  end
 end
